46e9342befbc2c17cea49c79b5824ee34dd36219
TimClifton <46601891+TimClifton@users.noreply.github.com>
2022-05-07 21:25:45 +1000

    Update

--- environment/settings.py
+++ environment/settings.py
@@ -3,7 +3,14 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
+if os.getenv("ENVIRONMENT_FILE") == None:
+
+    dotenv_path = join(dirname(__file__), '.env.development')
+
+else:
+
+    dotenv_path = join(dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
+
 load_dotenv(dotenv_path=dotenv_path, override=True)
 
 APP_HOST = os.environ.get("HOST")
